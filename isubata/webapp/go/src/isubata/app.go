494f63c849d368af48c1858117ea5c94ee3c81b6
naoto67 <uver.com67@gmail.com>
2019-05-19 12:19:21 +0900

    move jsonifyMessage in app to service

--- isubata/webapp/go/src/isubata/app.go
+++ isubata/webapp/go/src/isubata/app.go
@@ -251,22 +251,6 @@ func postMessage(c echo.Context) error {
 	return c.NoContent(204)
 }
 
-func jsonifyMessage(m Message) (map[string]interface{}, error) {
-	u := User{}
-	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
-		m.UserID)
-	if err != nil {
-		return nil, err
-	}
-
-	r := make(map[string]interface{})
-	r["id"] = m.ID
-	r["user"] = u
-	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
-	r["content"] = m.Content
-	return r, nil
-}
-
 func getMessage(c echo.Context) error {
 	userID := sessUserID(c)
 	if userID == 0 {
--- isubata/webapp/go/src/isubata/service.go
+++ isubata/webapp/go/src/isubata/service.go
@@ -83,3 +83,19 @@ func register(name, password string) (int64, error) {
 	}
 	return res.LastInsertId()
 }
+
+func jsonifyMessage(m Message) (map[string]interface{}, error) {
+	u := User{}
+	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
+		m.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	r := make(map[string]interface{})
+	r["id"] = m.ID
+	r["user"] = u
+	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
+	r["content"] = m.Content
+	return r, nil
+}
