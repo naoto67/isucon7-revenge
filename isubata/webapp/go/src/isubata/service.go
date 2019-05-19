6a4ad5a46736a6eb0e37e6615cddbb8975f9cc3e
naoto67 <uver.com67@gmail.com>
2019-05-19 11:43:53 +0900

    devide file

---
+++ isubata/webapp/go/src/isubata/service.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	"math/rand"
+
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
+)
+
+func getUser(userID int64) (*User, error) {
+	u := User{}
+	if err := db.Get(&u, "SELECT * FROM user WHERE id = ?", userID); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &u, nil
+}
+
+func addMessage(channelID, userID int64, content string) (int64, error) {
+	res, err := db.Exec(
+		"INSERT INTO message (channel_id, user_id, content, created_at) VALUES (?, ?, ?, NOW())",
+		channelID, userID, content)
+	if err != nil {
+		return 0, err
+	}
+	return res.LastInsertId()
+}
+
+func queryMessages(chanID, lastID int64) ([]Message, error) {
+	msgs := []Message{}
+	err := db.Select(&msgs, "SELECT * FROM message WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100",
+		lastID, chanID)
+	return msgs, err
+}
+
+func sessUserID(c echo.Context) int64 {
+	sess, _ := session.Get("session", c)
+	var userID int64
+	if x, ok := sess.Values["user_id"]; ok {
+		userID, _ = x.(int64)
+	}
+	return userID
+}
+
+func sessSetUserID(c echo.Context, id int64) {
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		HttpOnly: true,
+		MaxAge:   360000,
+	}
+	sess.Values["user_id"] = id
+	sess.Save(c.Request(), c.Response())
+}
+
+const LettersAndDigits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func randomString(n int) string {
+	b := make([]byte, n)
+	z := len(LettersAndDigits)
+
+	for i := 0; i < n; i++ {
+		b[i] = LettersAndDigits[rand.Intn(z)]
+	}
+	return string(b)
+}
+
+func register(name, password string) (int64, error) {
+	salt := randomString(20)
+	digest := fmt.Sprintf("%x", sha1.Sum([]byte(salt+password)))
+
+	res, err := db.Exec(
+		"INSERT INTO user (name, salt, password, display_name, avatar_icon, created_at)"+
+			" VALUES (?, ?, ?, ?, ?, NOW())",
+		name, salt, digest, name, "default.png")
+	if err != nil {
+		return 0, err
+	}
+	return res.LastInsertId()
+}
